c685a1f56fee87bf33b332d7ba6c8b525006d167
Gianluca Bianchin <44590401+gianlucaBi@users.noreply.github.com>
2023-05-09 10:26:50 +0200

    Update

---
+++ MAIN.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+jemdoc index 																			# This line compiles the main file
+python2.7 PUBLICATIONS/format_bibtex.py 1 												# This line converts the bib file into a jemdoc file
+jemdoc publications.jemdoc 															# This line compiles the publications jemdoc file
+jemdoc researchTopics.jemdoc 														# This line compiles the research topics jemdoc file
+jemdoc software.jemdoc 																# This line compiles the software jemdoc  file
+jemdoc teaching.jemdoc 																# This line compiles the teaching jemdoc  file
